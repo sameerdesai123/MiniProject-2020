0593e0cb3676e467b4484871da603286afa38cf3
sameerdesai123 <sameerdesai1008@gmail.com>
2020-04-28 17:35:05 +0530

    Added MNIST Design Model

--- Src/functions/index.js
+++ Src/functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.helloWorld = functions.https.onRequest(app);
